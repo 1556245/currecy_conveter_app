6d9c4dba5de0bb6bfab6f329b2d4e3f1b93acfd9
Mohamed Karam <mhmdmntf@gmail.com>
2024-07-01 19:34:14 +0300

    Initial commit

---
+++ app/src/main/java/com/mkaram/secondnativeproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mkaram.secondnativeproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
