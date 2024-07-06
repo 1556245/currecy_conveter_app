7419469c032c71a197a846c5b108c3364e7d141f
Mohamed Karam <mhmdmntf@gmail.com>
2024-07-06 23:00:52 +0300

    update From field

--- app/src/main/java/com/mkaram/secondnativeproject/MainActivity.kt
+++ app/src/main/java/com/mkaram/secondnativeproject/MainActivity.kt
@@ -3,55 +3,56 @@ package com.mkaram.secondnativeproject
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     val TAG: String = "MainActivity"
-    val EgyptionPoundvalue: String = "EgyptionPound"
-    val UAEvalue: String = "UAE"
-    val yyyyPoundvalue: String = "AmericanDollars"
-    val currencyList = listOf(EgyptionPoundvalue, UAEvalue, yyyyPoundvalue)
+    private val egyptionPound: String = "EgyptionPound"
+    private val uAEvalue: String = "UAE"
+    private val americanvalue: String = "AmericanDollar"
+    private val currencyList = listOf(egyptionPound, uAEvalue, americanvalue)
+
+    private lateinit var fromDropDownMenu: AutoCompleteTextView  // 2
+    private lateinit var toDropDownMenu: AutoCompleteTextView
+    private lateinit var convetBtn: Button
+    private lateinit var amountField: TextInputEditText
+    private lateinit var resultField: TextInputEditText
+
+    val map = mapOf("EgyptionPound" to 21.4, "UAE" to 7.4 , "AmericanDollar" to 1.0)
 
     @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val convetBtn: Button = findViewById(R.id.convert_button)
-        val amountField: TextInputEditText = findViewById(R.id.textfieldbyuser)
-        val resultField: TextInputEditText = findViewById(R.id.resultfield)
-        val adapter = ArrayAdapter(this, R.layout.dropdownlist_item, currencyList) // 1
-        val toDropDownMenu: AutoCompleteTextView = findViewById(R.id.select_dialog_listview) // 2
-        toDropDownMenu.setAdapter(adapter) //3
+
+        connectlayouts()
+        populateDropDownMenu()
 
         convetBtn.setOnClickListener {
-
-//            Log.d(TAG, "onCreate: ${toDropDownMenu.text}")
-            when (toDropDownMenu.text.toString()) { // 4
-                "EgyptionPound" -> resultField.setText(
-                    amountField.text.toString().toDouble().times(21.4).toString()
-                )
-
-                "UAE" -> resultField.setText(
-                    amountField.text.toString().toDouble().times(7.25).toString()
-                )
-
-                "AmericanDollars" -> resultField.setText(amountField.text.toString())
-
-            }
-
-
+            val amount: Double = amountField.text.toString().toDouble()
+            val toweight = map[toDropDownMenu.text.toString()]
+            val fromweight = map[fromDropDownMenu.text.toString()]
+            val result= (amount * toweight!!) / fromweight!!
+            resultField.setText(result.toString())
         }
 
     }
 
+    private fun populateDropDownMenu() {
+        val adapter = ArrayAdapter(this, R.layout.dropdownlist_item, currencyList)
+        toDropDownMenu.setAdapter(adapter)
+        fromDropDownMenu.setAdapter(adapter)
+    }
+
+    private fun connectlayouts() {
+        fromDropDownMenu = findViewById(R.id.select_dialog_listview1)
+        toDropDownMenu = findViewById(R.id.select_dialog_listview2)
+        convetBtn = findViewById(R.id.convert_button)
+        amountField = findViewById(R.id.textfieldbyuser)
+        resultField = findViewById(R.id.resultfield)
+    }
 }
